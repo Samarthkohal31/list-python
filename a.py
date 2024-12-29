88c60f928310e04dc381c4bfe81f9292c0a6f113
Samarth31# <deepikakohal27@gmail.com>
2024-12-29 17:55:26 +0400

    final

---
+++ a.py
@@ -0,0 +1,11 @@
+S=[31,4,1,7,9,10,1333333333333]
+print(len(S))
+print(S[4])
+print(S[1])
+S.append(74543512)
+print(S)
+S.remove(9)
+print(S)
+print(S[1:5])
+S.sort()
+print(S)
---
+++ b.py
@@ -0,0 +1,2 @@
+A={"name":"Samarth","age": 12,"hobby":"gaming"}
+print(A)
